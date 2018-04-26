30621ef38534f2d8a7aaddf174b2c5021f9cf909
Rafael Quines <rafael.quines@totvs.com.br>
2018-04-26 13:23:32 -0300

    dev

--- src/common/schemas/base.schema.ts
+++ src/common/schemas/base.schema.ts
@@ -1,5 +1,6 @@
 import * as mongoose from 'mongoose';
 import * as uuid from 'uuid';
+import * as util from 'util';
 
 export const BaseSchema = new mongoose.Schema({
     id: {
@@ -15,3 +16,24 @@ export const BaseSchema = new mongoose.Schema({
         default: null,
     },
 });
+
+// const BaseSchema = (args) => {
+//     mongoose.Schema.apply(this, arguments);
+//     this.add({
+//             id: {
+//                 type: String,
+//                 default: uuid.v4(),
+//             },
+//             deleted: {
+//                 default: false,
+//                 type: Boolean,
+//             },
+//             deletedAt: {
+//                 type: Date,
+//                 default: null,
+//             },
+//         });
+// };
+// util.inherits(BaseSchema, mongoose.Schema);
+
+// export default BaseSchema;
